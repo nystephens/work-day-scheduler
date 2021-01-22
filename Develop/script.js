78249b7aca789bd1a166f898780b294da2f691c9
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-01-21 16:55:41 -0800

    current time, task time, and task time limit variables set using moment.js

--- Develop/script.js
+++ Develop/script.js
@@ -2,23 +2,28 @@
 // Use moment.js to determine the current time of day.  This will be our currentTime variable.
 let currentTime = moment().format("X");
 console.log(currentTime);
+let taskTime = $("#task-time").text();
+console.log(taskTime);
+
+let taskTimeLimit = moment(taskTime, "h a").add(59, "m").add(59, "s").format("X");
+console.log(taskTimeLimit);
+console.log(moment(taskTimeLimit, "X").format("LTS"));
 
 // loadTasks(); this function will get the task data from localStorage and then load it into the textarea.  (This function will fire upon loading the page so it will towards the top.)
 
 
 // colorChange() function will be a for loop that iterates over the textarea elements and applies an if statement that adds the resective color coding class based on a comparison between the current time and the latest time in the hour designated for the row.
 
-let colorChange = function() {
+// let colorChange = function() {
+// // for loop starts here 
+// for (i = 0; i < 9; i++) {
+// // // If currentTime > the time in the row, then addClass .past  
+// // if (currentTime > )
+// // // if currentTime = the time in the row then addClass .present
 
-// for loop starts here 
-
-// If currentTime > the time in the row, then addClass .past  
-
-// if currentTime = the time in the row then addClass .present
-
-//  if current time < the time in the rown then addClass .future
-
-};
+// // //  if current time < the time in the rown then addClass .future
+// // }
+// };
 
 
 // saveTasks() function will set the text value of the text area as a data point in localStorage.
